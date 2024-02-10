cc0a4978181cb83f41255d22713f2a8686b4260f
srcrapi <rafa1000.skylanders@gmail.com>
2024-02-10 20:35:21 +0000

    chore: add a comment

--- HexReader/HexReader.cpp
+++ HexReader/HexReader.cpp
@@ -8,15 +8,15 @@ int main()
     std::cout << "Digite caracteres (Que vao aparecer em codigo hexadecimal)" << std::endl;
 
     do {
-        std::cin.get(caractere);
+        std::cin.get(caractere); // Le o input do usuário e coloca na variavél "caractere"
 
         if (caractere != '\n') {
             system("cls");
             std::cout << "Codigo hexadecimal: 0x" << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(caractere) << std::endl;
-            
+
         }
     } while (caractere != '0');
-   
+
     return 0;
 }
 
